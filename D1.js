d17047b6a4dd5ba0fcf4396fe73604bef8360223
Joselinedesouza <joselinedesouza022@gmail.com>
2025-02-10 17:51:12 +0100

    CORREZIONE

--- D1.js
+++ D1.js
@@ -88,4 +88,5 @@ name1 = "john";
 name2 = "John";
 
 console.log(name1 === name2);
-//console.log(name1!== name2);
+console.log(name1 !== name2);
+console.log(name1.toLowerCase() === name2.toLowerCase());
